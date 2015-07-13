934082a396d4aea8905261d6077c310912b1f115
jgarcy1080 <jgarcy1080@gmail.com>
2015-07-13 14:48:27 -0400

    !-Boston Assets
    `Created  a load screen and fixed the main menu

---
+++ Assets/1_BostonBuildAssets/Scripts/MenusLoadLevel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenusLoadLevel : MonoBehaviour {
+
+	private GameObject StartGame, ExitApp;
+
+	public Sprite SpriteSwap;
+	private SpriteRenderer spriteRenderer;
+
+	void Start (){
+				StartGame = GameObject.Find ("StartGame");
+				ExitApp = GameObject.Find ("ExitApp");
+				//StartCoroutine(Example());
+			spriteRenderer = GetComponent<SpriteRenderer>();
+		}
+
+
+	IEnumerator CountDown(){
+		yield return new WaitForSeconds (1);
+	}
+	void OnMouseDown() {
+		if (gameObject.name=="StartGame"){
+			spriteRenderer.sprite = SpriteSwap;
+			StartCoroutine(CountDown());{
+			Application.LoadLevel(1);
+			}
+
+}
+
+			else if(gameObject.name=="ExitApp"){
+			spriteRenderer.sprite = SpriteSwap;
+			Application.Quit();
+		}
+} 
+}
